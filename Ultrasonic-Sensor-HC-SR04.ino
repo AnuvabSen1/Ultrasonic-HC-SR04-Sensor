de05b35789cd48ff8344684daefc4f917bf64ff0
Anuvab Sen <sen.anuvab@gmail.com>
2021-08-18 19:38:15 +0530

    Create Ultrasonic-Sensor-HC-SR04.ino

---
+++ Ultrasonic-Sensor-HC-SR04.ino
@@ -0,0 +1,32 @@
+/*
+ * HC-SR04 example codes
+ *
+ * https://github.com/AnuvabSen/Ultrasonic-HC-SR04-Sensor/new/master
+ *
+ * by Anuvab Sen
+ */
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+}
